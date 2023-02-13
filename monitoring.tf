4e247274559bfcae8322de4dcc07ca237f33e7dd
arnobg <78933256+arnobg@users.noreply.github.com>
2023-02-13 18:48:39 +0000

    Added monitoring.tf

---
+++ monitoring.tf
@@ -0,0 +1,69 @@
+
+// Cloudwatch Alarms
+
+resource "aws_cloudwatch_metric_alarm" "cloudfront-500-errors" {
+  alarm_name          = var.alarm_500_name
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "5xxErrorRate"
+  namespace           = "AWS/CloudFront"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 5
+  treat_missing_data  = "notBreaching"
+  alarm_actions       = [aws_sns_topic.sns_error_topic.arn]
+  actions_enabled     = true
+
+  dimensions = {
+    DistributionId = aws_cloudfront_distribution.s3_distribution.id
+    Region         = "Global"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cloudfront-request-alert" {
+  alarm_name          = var.alarm_req_name
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Requests"
+  namespace           = "AWS/CloudFront"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 0
+  treat_missing_data  = "notBreaching"
+  alarm_actions       = [aws_sns_topic.sns_alert_topic.arn]
+  actions_enabled     = true
+
+  dimensions = {
+    DistributionId = aws_cloudfront_distribution.s3_distribution.id
+    Region         = "Global"
+  }
+}
+
+
+// SNS Topic/subscriber with CW 500 error metric alarm
+
+resource "aws_sns_topic" "sns_error_topic" {
+  name = "sns_error_topic"
+}
+
+
+resource "aws_sns_topic_subscription" "error_topic_sms_subscription" {
+  count     = length(var.phone_numbers)
+  topic_arn = aws_sns_topic.sns_error_topic.arn
+  protocol  = "sms"
+  endpoint  = var.phone_numbers[count.index]
+}
+
+// SNS Topic/subscriber with CW Request alert metric alarm
+
+resource "aws_sns_topic" "sns_alert_topic" {
+  name = "sns_alert_topic"
+}
+
+
+resource "aws_sns_topic_subscription" "alert_topic_sms_subscription" {
+  count     = length(var.phone_numbers)
+  topic_arn = aws_sns_topic.sns_alert_topic.arn
+  protocol  = "sms"
+  endpoint  = var.phone_numbers[count.index]
+}
